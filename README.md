23bd6aa47fdc3a76c0a20dc01486f889f6a97aa2
Oleksii Gryniuk <mr.neuton@gmail.com>
2015-04-29 00:02:48 +0300

    some major rewritings; should at least tunnel https now and work better; readme upd

--- README.md
+++ README.md
@@ -9,34 +9,54 @@ Usage
 TODO
 ----------
 - somehow signal a client browser (I guess that's the solution) about problems with outgoing connection to server (test deviantart.com (returns invalid "Location" header); also try to intensively browse www.deviantart.com)
-- make nice logging
+- actually, www.deviantart.com doesn't work anymore at all
+- as well as www.wikipedia.com
+- while en.wikipedia.org works only partially
+- ...
+- maybe make nice logging
 
 Filtering usage example
 ----------
 ```python
 #!/usr/bin/env python
 
-from http import HttpResponse
+from traceback import print_exc
+from http import HttpRequest, HttpResponse
 from proxy import parse_host_port
 import proxy, sys
 
-def filter_response(caddr, saddr, response):
-	if '.'.join(saddr[0].split('.')[-2:]) == 'example.com':
-		try:
+filter_hosts = ['example.com', 'example.org']
+
+def filter_request(request):
+	try:
+		meta = request.get_meta()
+		if meta.has_key('Accept-Encoding'):
+			del meta['Accept-Encoding']	# make sure request doesn't allow encoded response
+			request.set_meta(meta)
+	except:
+		print_exc()
+	return request
+
+def filter_response(request, response):
+	try:
+		if request.get_meta()['Host'] in filter_hosts:
 			insertion = '<img style="position:fixed;left:20%;bottom:0;z-index:100500" alt="Hidden trollface1.png" src="//lurkmore.so/images/8/80/Hidden_trollface1.png" width="192" height="56">'
 			meta = response.get_meta()
 			if meta.has_key('Content-Type') and 'text/html' in meta['Content-Type'].lower():
-				meta['Content-Length'] = str(int(meta['Content-Length']) + len(insertion))
+				if meta.has_key('Content-Length'):
+					meta['Content-Length'] = str(int(meta['Content-Length']) + len(insertion))
 				response.set_meta(meta)
 				body = response.get_body()
-				i = body.lower().rfind('</html>')
+				find_tag = '<body>'
+				i = body.lower().find(find_tag) + len(find_tag)
 				response.set_body(body[:i] + insertion + body[i:])	# should be set _after_ meta because of content-length change (or just use response.set(...) to set both simultaneously)
-				print '--- insertion done'
-		except Exception as e:
-			print e
+				print '>-< insertion done'
+	except:
+		print_exc()
 	return response
 
 if __name__ == '__main__':
+	proxy.filter_request = filter_request
 	proxy.filter_response = filter_response
 	proxy.Server(*parse_host_port(' '.join(sys.argv[1:]), 'localhost', 8080)).run()
 ```
--- proxy.py
+++ proxy.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 
+#from traceback import print_exc
 import socket, multiprocessing as mp, sys, time
 from http import HttpRequest, HttpResponse
 
 
-def filter_request(client_addr, server_addr, request):
+valid_request_methods = ['OPTIONS', 'GET', 'HEAD', 'POST', 'PUT', 'PATCH', 'DELETE', 'TRACE', 'CONNECT']
+
+
+def filter_request(request):
 	return request
 
 
-def filter_response(client_addr, server_saddr, response):
+def filter_response(request, response):
 	return response
 
 
@@ -25,104 +29,174 @@ def parse_host_port(string, default_host=None, default_port=None):
 	return host or default_host, port or default_port
 
 
-def conn_str(addr1, addr2):
-	return addr1[0] + ':' + str(addr1[1]) + ' --> ' + addr2[0] + ':' + str(addr2[1])
+def conn_str(addr1, addr2, sep=' --> '):
+	return addr1[0] + ':' + str(addr1[1]) + sep + addr2[0] + ':' + str(addr2[1])
+
+
+def new_socket():
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+	return s
+
+
+def socket_is_connected(s):
+	if s is not None:
+		try:
+			s.getpeername()
+			return True
+		except socket.error:
+			return False
+
+
+def try_close_socket(s):
+	if s is not None:
+		try:
+			s.shutdown(socket.SHUT_RDWR)
+			s.close()
+		except socket.error:
+			pass
+
+
+def http_should_keep_alive(http):
+	meta = http.get_meta()
+	if http.get_version() == '1.1':
+		return not (meta.has_key('Connection') and meta['Connection'] == 'close')
+	else:
+		return meta.has_key('Connection') and meta['Connection'] == 'keep-alive'
 
 
 class CommunicationProcess(mp.Process):
 	
-	def __init__(self, s1, s2, bufsize=4096):
+	def __init__(self, s1, s2, bufsize=65535):
 		mp.Process.__init__(self)
 		self.s1, self.s2, self.bufsize = s1, s2, bufsize
 	
 	def run(self):
-		while True:
-			r = self.s1.recv(self.bufsize)
-			if not r:
-				break
-			self.s2.sendall(r)
+		try:
+			while True:
+				r = self.s1.recv(self.bufsize)
+				if not r:
+					break
+				self.s2.sendall(r)
+		except:
+			pass
 
 
 class ClientProcess(mp.Process):
 	
-	def __init__(self, s, si, bufsize=4096):
+	def __init__(self, client_socket, bufsize=65535):
 		mp.Process.__init__(self)
-		self.s = s
-		self.si = si
-		self.so = None
+		self.client_socket = client_socket
+		self.server_socket = None
 		self.bufsize = bufsize
-		self.client_addr = si.getpeername()
-		self.server_addr = None
 	
 	def run_tunnel(self):
-		print '--- TCP tunnel established'
-		cp = CommunicationProcess(self.so, self.si, bufsize=self.bufsize)
+		cp = CommunicationProcess(self.server_socket, self.client_socket, bufsize=self.bufsize)
 		cp.start()
-		CommunicationProcess(self.si, self.so, bufsize=self.bufsize).run()
-		cp.terminate()
+		try:
+			while True:
+				r = self.client_socket.recv(self.bufsize)
+				if not r:
+					break
+				self.server_socket.sendall(r)
+		except socket.error:
+			pass
+		cp.join()
+	
+	def recv_http(self, s, http):
+		while not http.is_complete():
+			r = s.recv(self.bufsize)
+			if not r:
+				raise socket.error, 'Connection closed unexpectedly'
+			http.append(r)
+		return http
+	
+	def recv_request(self):
+		s = self.client_socket
+		r = s.recv(self.bufsize)
+		if not r:
+			raise socket.error, 'Connection closed unexpectedly'
+		m = r.split()[0].upper()
+		if m not in map(lambda a: a[:len(m)], valid_request_methods): # if request is definitely invalid
+			return None
+		request = HttpRequest()
+		request.append(r)
+		return self.recv_http(s, request)
+	
+	def recv_response(self):
+		s = self.server_socket
+		r = s.recv(self.bufsize)
+		if not r:
+			raise socket.error, 'Connection closed unexpectedly'
+		#m = r.split()[0].upper()
+		#if m[:4] != 'HTTP'[:len(m)]: # if response is definitely invalid
+		#	return None
+		response = HttpResponse()
+		response.append(r)
+		return self.recv_http(s, response)
+	
+	def send_http(self, s, http):
+		s.sendall(http.get_raw())
+	
+	def send_response(self, response):
+		self.send_http(self.client_socket, response)
+	
+	def send_request(self, request):
+		self.send_http(self.server_socket, request)
+	
+	def set_server(self, host):
+		c_addr = self.client_socket.getpeername()
+		s_addr = parse_host_port(host, default_port=80)
+		if s_addr == self.client_socket.getsockname():	# prevent self-nuke
+			raise socket.error, "Can't proxy to self!"
+		if socket_is_connected(self.server_socket):
+			s_addr_0 = self.server_socket.getpeername()
+			if s_addr != s_addr_0:
+				try_close_socket(self.server_socket)
+				print '[-] ' + conn_str(c_addr, s_addr_0)
+		self.server_socket = new_socket()
+		self.server_socket.connect(s_addr)
+		print '[+] ' + conn_str(c_addr, s_addr)
 	
 	def run(self):
 		try:
 			while True:
-				request = HttpRequest()
-				while not request.is_complete():
-					r = self.si.recv(self.bufsize)
-					if not r:
-						raise socket.error
-					request.append(r)
-				meta = request.get_meta()
-				if meta.has_key('Host'):
-					server_addr = parse_host_port(meta['Host'], default_port=80)
-					if server_addr == self.s.getsockname():	# prevent self-nuke
-						raise socket.error
-					if server_addr != self.server_addr:
-						if self.so is not None:
-							print '[-] ' + conn_str(self.client_addr, self.server_addr)
-							self.so.shutdown(socket.SHUT_RDWR)
-							self.so.close()
-						print '[+] ' + conn_str(self.client_addr, server_addr)
-						self.server_addr = server_addr
-				elif self.so is None:
-					raise socket.error
-				self.so = socket.socket()
-				self.so.connect(server_addr)
-				self.so.sendall(filter_request(self.client_addr, server_addr, request).get_raw())
-				response = HttpResponse()
-				while not response.is_complete():
-					r = self.so.recv(self.bufsize)
-					if not r:
-						break
-					response.append(r)
-				self.si.sendall(filter_response(self.client_addr, server_addr, response).get_raw())
-				if request.get_method() == 'CONNECT' and response.get_status_comment() == 'OK':
+				req = self.recv_request()
+				if req is None:
+					raise socket.error, 'Invalid HTTP request'
+				if req.get_method() == 'CONNECT':
+					self.send_response(HttpResponse(raw='HTTP/1.1 200 OK\r\n\r\n'))
+					self.set_server(req.get_path())
 					self.run_tunnel()
-				rmeta = response.get_meta()
-				keep_alive = meta.has_key('Connection') and meta['Connection'] == 'keep-alive'
-				keep_alive = keep_alive and meta.has_key('Proxy-Connection') and meta['Proxy-Connection'] == 'keep-alive'
-				keep_alive = keep_alive and rmeta.has_key('Connection') and rmeta['Connection'] == 'keep-alive'
-				if not keep_alive:
-					self.clean()
+					break
+				headers = req.get_meta()
+				if headers.has_key('Host'):
+					try:
+						socket.gethostbyname(headers['Host'])
+					except socket.error:
+						self.send_response(HttpResponse(raw='HTTP/1.1 502 DNS Lookup Failed\r\nConnection: close\r\n\r\n'))
+						break
+					self.set_server(headers['Host'])
+				elif not socket_is_connected(self.server_socket):
+					raise socket.error, 'No "Host" header specified in request'
+				self.send_request(filter_request(req))
+				resp = self.recv_response()
+				self.send_response(filter_response(req, resp))
+				if not http_should_keep_alive(req) or not http_should_keep_alive(resp):
 					break
 		except KeyboardInterrupt:
-			self.clean()
-		except Exception as e:
+			pass
+		except Exception, e:
 			if str(e).strip():
 				print e
-			self.clean()
+			#print_exc()
+		self.clean()
 	
 	def clean(self):
-		if self.so is not None:
-			print '[-] ' + conn_str(self.client_addr, self.server_addr)
-			try:
-				self.so.shutdown(socket.SHUT_RDWR)
-				self.so.close()
-			except:
-				pass
-		try:
-			self.si.shutdown(socket.SHUT_RDWR)
-			self.si.close()
-		except:
-			pass
+		if socket_is_connected(self.client_socket) and socket_is_connected(self.server_socket):
+			print '[-] ' + conn_str(self.client_socket.getpeername(), self.server_socket.getpeername())
+		try_close_socket(self.client_socket)
+		try_close_socket(self.server_socket)
 
 
 class Server(mp.Process):
@@ -134,28 +208,22 @@ class Server(mp.Process):
 	
 	def run(self):
 		try:
-			self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-			self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+			self.s = new_socket()
 			self.s.bind((self.host, self.port))
 			self.s.listen(5)
 			print 'Listening ' + self.host + ':' + str(self.port) + '...'
 			while True:
 				cs, addr = self.s.accept()
-				ClientProcess(self.s, cs).start()
+				ClientProcess(cs).start()
 		except KeyboardInterrupt:
-			self.clean()
+			pass
 		except Exception as e:
 			if str(e).strip():
 				print e
-			self.clean()
+		self.clean()
 	
 	def clean(self):
-		print 'Shutting down...'
-		try:
-			self.s.shutdown(socket.SHUT_RDWR)
-			self.s.close()
-		except:
-			pass
+		try_close_socket(self.s)
 
 
 if __name__ == '__main__':
